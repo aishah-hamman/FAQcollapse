4db7510f96d5422025ae94e7da2d13b8b2496b0d
aishah-hamman <140021931+aishah-hamman@users.noreply.github.com>
2023-10-24 19:32:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle=>{
+	toggle.addEventListener('click', () =>{
+		toggle.parentNode.classList.toggle('active')
+	})
+})
+
+
+// const toggles = document.querySelectorAll('.faq-toggle')
+
+// toggles.forEach(toggle=>{
+// 	toggle.addEventListener('click', () =>{
+// 		toggle.parentNode.classList.toggle('active')
+// 	})
+// })
+
+// const toggles = document.querySelectorAll('.faq-toggle')
+
+// toggles.forEach(toggle=>{
+// 	toggle.addEventListener('click', () =>{
+// 		toggle.parentNode.classList.toggle('active')
+// 	})
+// })
+
+
